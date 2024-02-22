6a59cf78f182f9bfadfd47107923f957da206db2
flo0web <flo0.webmaster@gmail.com>
2024-02-22 15:39:29 +0300

    model fix

--- mplace/ozon/apps/transactions.py
+++ mplace/ozon/apps/transactions.py
@@ -6,8 +6,15 @@ from mplace.ozon import Client, Transaction
 def sync(date_from, date_to, client: Client, session: Session):
     """Записывает Список транзакций за период date_from - date_to"""
 
-    items: [Transaction] = client.get_transactions(date_from=date_from, date_to=date_to)
-    for item in items:
-        session.merge(item)
+    transactions: [Transaction] = client.get_transactions(date_from=date_from, date_to=date_to)
+    for transaction in transactions:
+        known_transaction: [Transaction] = session.query(Transaction).get(transaction.operation_id)
+
+        if known_transaction is not None:
+            continue
+
+        transaction.items = [session.merge(item) for item in transaction.items]
+
+        session.add(transaction)
 
     session.commit()
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setuptools.setup(
     name="mplace",
-    version="1.3.3",
+    version="1.3.4",
     author="mrkisa",
     description="",
     install_requires=REQUIREMENTS,
